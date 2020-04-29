72017cc81e72045e42bdb6e7a2292164984c7799
Sohrab Namazi <sohrabsn7@yahoo.com>
2020-04-30 01:48:09 +0430

    Second Dataset

--- c#/Lufthansa/Lufthansa/Program.cs
+++ c#/Lufthansa/Lufthansa/Program.cs
@@ -15,55 +15,55 @@ namespace Lufthansa
 
         static void Main(string[] args)
         {
-            List<Airport> airports = GetAirports();
-            List<City> cities = GetCities();
+            //List<Airport> airports = GetAirports();
+            //List<City> cities = GetCities();
         }
 
-        private static List<City> GetCities()
-        {
-            XmlDocument doc = new XmlDocument();
-            //Change it
-            doc.Load("C:\\Users\\Sohrab\\Desktop\\Lufthansa\\Lufthansa\\Resources\\City.xml");
-            List<City> cities = new List<City>();
-            XmlNode idNodes = doc.SelectSingleNode("CityResource/Cities");
-            foreach (XmlNode node in idNodes.ChildNodes)
-            {
-                City city = ConvertNode<City>(node);
-                cities.Add(city);
-            }
-            return cities;
-        }
+        //    private static List<City> GetCities()
+        //    {
+        //        XmlDocument doc = new XmlDocument();
+        //        //Change it
+        //        doc.Load("C:\\Users\\Sohrab\\Desktop\\Lufthansa\\Lufthansa\\Resources\\City.xml");
+        //        List<City> cities = new List<City>();
+        //        XmlNode idNodes = doc.SelectSingleNode("CityResource/Cities");
+        //        foreach (XmlNode node in idNodes.ChildNodes)
+        //        {
+        //            City city = ConvertNode<City>(node);
+        //            cities.Add(city);
+        //        }
+        //        return cities;
+        //    }
 
-        private static List<Airport> GetAirports()
-        {
-            XmlDocument doc = new XmlDocument();
-            //Change it
-            doc.Load("C:\\Users\\Sohrab\\Desktop\\Lufthansa\\Lufthansa\\Resources\\Airports.xml");
-            List<Airport> airports = new List<Airport>();
-            XmlNode idNodes = doc.SelectSingleNode("AirportResource/Airports");
-            foreach (XmlNode node1 in idNodes.ChildNodes)
-            {
-                Airport airport = ConvertNode<Airport>(node1);
-                airports.Add(airport);
-            }
+        //    private static List<Airport> GetAirports()
+        //    {
+        //        XmlDocument doc = new XmlDocument();
+        //        //Change it
+        //        doc.Load("C:\\Users\\Sohrab\\Desktop\\Lufthansa\\Lufthansa\\Resources\\Airports.xml");
+        //        List<Airport> airports = new List<Airport>();
+        //        XmlNode idNodes = doc.SelectSingleNode("AirportResource/Airports");
+        //        foreach (XmlNode node1 in idNodes.ChildNodes)
+        //        {
+        //            Airport airport = ConvertNode<Airport>(node1);
+        //            airports.Add(airport);
+        //        }
 
-            return airports;
-        }
+        //        return airports;
+        //    }
 
-        private static T ConvertNode<T>(XmlNode node) where T : class
-        {
-            MemoryStream stm = new MemoryStream();
+        //    private static T ConvertNode<T>(XmlNode node) where T : class
+        //    {
+        //        MemoryStream stm = new MemoryStream();
 
-            StreamWriter stw = new StreamWriter(stm);
-            stw.Write(node.OuterXml);
-            stw.Flush();
+        //        StreamWriter stw = new StreamWriter(stm);
+        //        stw.Write(node.OuterXml);
+        //        stw.Flush();
 
-            stm.Position = 0;
+        //        stm.Position = 0;
 
-            XmlSerializer ser = new XmlSerializer(typeof(T));
-            T result = (ser.Deserialize(stm) as T);
+        //        XmlSerializer ser = new XmlSerializer(typeof(T));
+        //        T result = (ser.Deserialize(stm) as T);
 
-            return result;
-        }
+        //        return result;
+        //    }
     }
 }
